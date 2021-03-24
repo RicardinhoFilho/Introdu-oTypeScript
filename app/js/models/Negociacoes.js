62d0a6ebc3de908b14f80d449babe342c883a163
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-03-24 15:33:05 -0300

    Adicionando JQuery e desabilitando co comentário de TS para JS

--- app/js/models/Negociacoes.js
+++ app/js/models/Negociacoes.js
@@ -1,12 +1,11 @@
 class Negociacoes {
     constructor() {
-        //private _negociacoes: Array<Negociacao> = [];
         this._negociacoes = [];
     }
     Adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
     ParaArray() {
-        return [].concat(this._negociacoes); //retornamos uma cópia de nosso array, o tornando imútavel
+        return [].concat(this._negociacoes);
     }
 }
